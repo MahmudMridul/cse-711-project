ba86922a0794520f95b22cff7ed2f35001975573
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-12-26 16:16:05 +0600

    rename file

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
-file = 'data/combined_hospital_dataset_2020_2024.csv'
+file = 'data/hospital_admission_with_weather.csv'
 
 df = pd.read_csv(file)
-
-# info.get_categorical_data_info(df)
-
-df['admission_date'] = df['admission_date'].str[:10]
-
-df.to_csv('data/hospital_admission_no_timestamp.csv', index=False)
+info.get_categorical_data_info(df)
