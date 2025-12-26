e5493f3784cb92e5f2433a186cc5f3fe980cc6b7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-12-26 12:39:54 +0600

    compare the two datasets

--- comparison.py
+++ comparison.py
@@ -8,16 +8,18 @@ file_1 = 'data/Hospital_Dataset_2020_2024.csv'
 file_2 = 'data/Hospital_Dataset_2022_2024.csv'
 
 
-df1 = pd.read_csv(file_1, parse_dates=['admission_date'])
-df2 = pd.read_csv(file_2, parse_dates=['admission_date'])
+df1 = pd.read_csv(file_1)
+# df1 = pd.read_csv(file_1, parse_dates=['admission_date'])
+df2 = pd.read_csv(file_2)
+# df2 = pd.read_csv(file_2, parse_dates=['admission_date'])
 
-overlapping_dates = df1[df1['admission_date'].isin(df2['admission_date'])]['admission_date'].unique()
+# overlapping_dates = df1[df1['admission_date'].isin(df2['admission_date'])]['admission_date'].unique()
 
-if len(overlapping_dates) > 0:
-    print(f"Warning: Found {len(overlapping_dates)} overlapping dates!")
-    print(overlapping_dates)
-else:
-    print("Success: No overlapping dates found.")
+# if len(overlapping_dates) > 0:
+#     print(f"Warning: Found {len(overlapping_dates)} overlapping dates!")
+#     print(overlapping_dates)
+# else:
+#     print("Success: No overlapping dates found.")
 
 common = pd.merge(df1, df2, how='inner')
 print(f'{len(common)} common rows found between the two datasets.')
